a4dad7ec2a4272c8457f38efe395cbe27ab5e9d9
Willem Meints <willem.meints@infosupport.com>
2018-03-08 16:14:40 -0800

    Add sample bot

---
+++ examples/BasicBot/EchoFlowProvider.cs
@@ -0,0 +1,30 @@
+﻿using Akka;
+using Akka.Streams.Dsl;
+using Gadget.Core;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicBot
+{
+    public class EchoFlowProvider : IDialogFlowProvider
+    {
+        public Flow<IActivity, IEnumerable<IActivity>, NotUsed> GetDialogFlow()
+        {
+            return Flow.FromFunction<IActivity, IEnumerable<IActivity>>(activity =>
+            {
+                if (activity.Type == ActivityTypes.Message)
+                {
+                    return new[]
+                    {
+                        ((Activity)activity).CreateReply(activity.AsMessageActivity().Text)
+                    };
+                }
+
+                return new Activity[] { };
+            });
+        }
+    }
+}
---
+++ examples/BasicBot/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gadget.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BasicBot
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGadgetBot<EchoFlowProvider>()
+                .UseCredentialProvider(new ConfigurationCredentialProvider(Configuration));
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGadgetBot(new GadgetBotMiddlewareOptions(new ConfigurationCredentialProvider(Configuration)));
+        }
+    }
+}
